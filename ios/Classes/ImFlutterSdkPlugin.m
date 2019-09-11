146ea1be2f99828973b76d712d550fea57e12c95
dujiepeng <347302029@qq.com>
2019-09-11 20:30:08 +0800

    add em_sdk_plugin.dart file

--- ios/Classes/ImFlutterSdkPlugin.m
+++ ios/Classes/ImFlutterSdkPlugin.m
@@ -10,8 +10,9 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  // ??
+  if ([@"EMClient.getInstance().init()" isEqualToString:call.method]) {
+    result(@"");
   } else {
     result(FlutterMethodNotImplemented);
   }
---
+++ lib/src/em_sdk_plugin.dart
@@ -0,0 +1,3 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
