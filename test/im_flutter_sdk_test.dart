4d6a9cd8ded5a793730ddc626d9db3052a68b79f
bingo <bingo.zhao@gmail.com>
2019-09-19 12:40:08 +0800

    add test case for login callback

--- test/im_flutter_sdk_test.dart
+++ test/im_flutter_sdk_test.dart
@@ -1,23 +1,26 @@
-
 import 'package:flutter_test/flutter_test.dart';
 
 import '../lib/im_flutter_sdk.dart';
 
 void main() {
   // setup EMClient
-    EMOptions options = EMOptions(appKey: 'easemob#im_demo');
-    EMClient client = EMClient.getInstance();
-  
+  EMOptions options = EMOptions(appKey: 'easemob#im_demo');
+  EMClient client = EMClient.getInstance();
+
   setUp(() {
     client.init(options);
   });
 
-  test('client listeners get called once migrate to 2x',
-  () {
-    
+  test('client listeners get called once migrate to 2x', () {});
+
+  test('client login with callback invoked correctly', () {
+    client.login('user1', 'passw0rd', onSuccess: () {
+      print('login success');
+    });
+    client.login('user2', 'passw0rd', onError: (int code, String status) {
+      print('login error with code: $code, status: $status');
+    });
   });
 
-  tearDown(() {
-   
-  });
+  tearDown(() {});
 }
