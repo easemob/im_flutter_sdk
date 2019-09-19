e0d7757a8061ac704ee1bdcc7867899f10236766
Bingo Zhao <bingo.zhao@gmail.com>
2019-09-19 09:56:25 +0800

    refactor listener/callback design as Effectiv Dart...

--- lib/src/em_client.dart
+++ lib/src/em_client.dart
@@ -6,9 +6,9 @@ import 'em_domain_terms.dart';
 import 'em_sdk_method.dart';
 
 class EMClient {
-  static const channelPrefix = 'com.easemob.im';
+  static const _channelPrefix = 'com.easemob.im';
   static const MethodChannel _emClientChannel =
-      const MethodChannel('$channelPrefix/em_client');
+      const MethodChannel('$_channelPrefix/em_client');
 
   final EMChatManager _chatManager = EMChatManager();
   final EMContactManager _contactManager = EMContactManager();
@@ -16,7 +16,7 @@ class EMClient {
   static EMClient _instance;
   EMClient._internal() {
     //init event channel
-    _clientEventChannel.receiveBroadcastStream().listen(_onClientEvent);
+    _clientEventChannel.receiveBroadcastStream().listen(_onMigrate2x);
     _connectionEventChannel.receiveBroadcastStream().listen(_onConnectionEvent);
     _loginCallbackEventChannel
         .receiveBroadcastStream()
@@ -31,31 +31,42 @@ class EMClient {
     _emClientChannel.invokeMethod(EMSDKMethod.Init, {"appkey": options.appKey});
   }
 
-  final _loginCallback = List<EMCallback>();
+  final _loginSuccessCallbacks = List<Success>();
+  final _loginErrorCallbacks = List<Error>();
+  final _loginProgressCallbacks = List<Progress>();
   final _loginCallbackEventChannel =
-      EventChannel('$channelPrefix/login_callback');
+      EventChannel('$_channelPrefix/login_callback');
 
   /// login - login server with username/password.
   void login(
-      final String id, final String password, final EMCallback callback) {
+      final String id, final String password, {onSuccess: Success, onError: Error, onProgress: Progress}) {
     // only 1 login callback at once
-    _loginCallback.clear();
-    _loginCallback.add(callback);
-    _emClientChannel.invokeMethod(EMSDKMethod.Login, {id: id, password: password});
+    _loginSuccessCallbacks.add(onSuccess);
+    _loginErrorCallbacks.add(onError);
+    _loginProgressCallbacks.add(onProgress);
+   
+    _emClientChannel
+        .invokeMethod(EMSDKMethod.Login, {id: id, password: password});
   }
 
   void _onLoginEvent(Object event) {
     if (event == 'success') {
-      _loginCallback.forEach((callback) => callback.onSuccess());
+      for (var callback in _loginSuccessCallbacks) {
+        callback();
+      }
     } else if (event == 'progress') {
       // TODO: get progess/status from event object.
-      _loginCallback.forEach((callback) => callback.onProgress(25, '%25'));
+      for (var callback in _loginProgressCallbacks) {
+        callback(25, '%25');
+      }
     }
   }
 
   void _onLoginError(Object error) {
     // TODO： get code/status from error object.
-    _loginCallback.forEach((callback) => callback.onError(1, 'error'));
+    for (var callback in _loginErrorCallbacks) {
+      callback(1, 'error');
+    }
   }
 
   EMChatManager chatManager() {
@@ -67,69 +78,52 @@ class EMClient {
   }
 
   /// Event handler methods.
-  void _onClientEvent(Object event) {
+  void _onMigrate2x(Object event) {
     if (event == 'migrate2x') {
       //TODO: onMigrate2X's variable from event
-      _clientListeners.forEach((listener) => listener.onMigrate2x(true));
+      for (var callback in _migrate2xCallbacks) {
+        callback(true);
+      }
     }
   }
 
   void _onConnectionEvent(Object event) {
     if (event == 'connected') {
-      _connectionListeners.forEach((listener) => listener.onConnected());
+      for (var callback in _connectionConnectedCallbacks) {
+        callback();
+      }
     } else if (event == 'disconnected') {
       // TODO: get error code variable from event
-      _connectionListeners.forEach((listener) => listener.onDisconnected(1));
+      for (var callback in _connectionDisconnectedCallbacks) {
+        callback(1);
+      }
     }
   }
 
-  final _clientListeners = List<EMClientListener>();
-  final _clientEventChannel = EventChannel('$channelPrefix/client');
+  final _migrate2xCallbacks = List<Migrate2x>();
+  final _clientEventChannel = EventChannel('$_channelPrefix/client');
 
-  /// addClientListener - add [EMClientListener] into listners list.
-  void addClientListener(final EMClientListener listener) {
-    _clientListeners.add(listener);
+  /// onMigrate2x - SDK EMClient.addClientListener().
+  void onMigrate2x(final Migrate2x callback) {
+    _migrate2xCallbacks.add(callback);
   }
 
-  /// removeClientListener - remove [EMClientLister] from list.
-  void removeClientListener(final EMClientListener listener) {
-    var idx = _clientListeners.indexOf(listener);
-    if (idx != -1) {
-      _clientListeners.removeAt(idx);
-    }
-  }
-
-  final _connectionListeners = List<EMConnectionListener>();
-  final _connectionEventChannel = EventChannel('$channelPrefix/connection');
-
-  /// addConnectionListener - add [EMConnectionListener] into listners list.
-  void addConnectionListener(final EMConnectionListener listener) {
-    _connectionListeners.add(listener);
-  }
-
-  /// removeConnectionListener - remove [EMConnectionLister] from list.
-  void removeConnectionListener(final EMConnectionListener listener) {
-    var idx = _connectionListeners.indexOf(listener);
-    if (idx != -1) {
-      _connectionListeners.removeAt(idx);
-    }
-  }
+  final _connectionConnectedCallbacks = List<Connected>();
+  final _connectionDisconnectedCallbacks = List<Disconnected>();
+  final _connectionEventChannel = EventChannel('$_channelPrefix/connection');
 }
 
-/// EMClientListener - listener receiving client migration events.
-abstract class EMClientListener {
-  void onMigrate2x(final bool success);
-}
+/// Migrate2x - SDK EMClientListener.onMigrate2x() function.
+typedef void Migrate2x(bool success);
 
-/// EMConnectionListener - listener receiving connect/disconnect events.
-abstract class EMConnectionListener {
-  void onConnected();
-  void onDisconnected(final int errorCode);
-}
+/// Connected - SDK EMConnectionListener.onConnected() to react upon connection connected.
+typedef void Connected();
+/// Disconnected - SDK EMConnectionListener.onDisconnected() to react upon conection disconnected.
+typedef void Disconnected(int errorCode);
 
-/// EMCallback - callback functions.
-abstract class EMCallback {
-  void onSuccess();
-  void onError(final int code, final String error);
-  void onProgress(final int progress, final String status);
-}
+/// Success - SDK EMCallback.onSuccess() callback.
+typedef void Success();
+/// Error - SDK EMCallback.onError() callback.
+typedef void Error(int code, String error);
+/// Progress - SDK EMCallback.onProgress() callback.
+typedef void Progress(int progress, String status);
