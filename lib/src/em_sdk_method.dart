dce2f0d335f2732867abc0ad40440f07d9eabe1b
bingo <bingo.zhao@gmail.com>
2019-09-19 19:31:34 +0800

    start implmenting methods in batch way

--- lib/src/em_sdk_method.dart
+++ lib/src/em_sdk_method.dart
@@ -1,7 +1,14 @@
 class EMSDKMethod {
-  //TODO: to add prefix EMClient.Init identifying callee?
-  static const String Init = 'EMClient.getInstance().init()';
-  static const String ChatManager = 'chatManager';
-  static const String Login = 'login';
+  static const String init = 'init';
+  static const String chatManager = 'chatManager';
+  static const String login = 'login';
+  static const String loginWithToken = 'loginWithToken';
+  static const String logout = 'logout';
+  static const String changeAppKey = 'changeAppKey';
+  static const String getCurrentUser = 'getCurrentUser';
+  static const String getUserTokenFromServer = 'getUserTokenFromServer';
+  static const String setDebugMode = 'setDebugMode';
+  static const String updateCurrentUserNick = 'updateCurrentUserNick';
+
   static const String SendMessage = 'sendMessage';
 }
