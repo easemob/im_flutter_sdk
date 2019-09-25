e9d2a6756d4cc8853a762259799fcbf229cd6561
bingo <bingo.zhao@gmail.com>
2019-09-25 13:23:18 +0800

    define EMClient interface, to adjust ios/android implementation accordingly

--- lib/src/em_sdk_method.dart
+++ lib/src/em_sdk_method.dart
@@ -1,18 +1,18 @@
 class EMSDKMethod {
+  static const String init = 'init';
+  static const String createAccount = 'createAccount';
+  static const String login = 'login';
+  static const String loginWithToken = 'loginWithToken';
+  static const String logout = 'logout';
+  static const String chatManager = 'chatManager';
+  static const String register = 'register';
+  static const String changeAppKey = 'changeAppKey';
+  static const String getCurrentUser = 'getCurrentUser';
+  static const String getUserTokenFromServer = 'getUserTokenFromServer';
+  static const String setDebugMode = 'setDebugMode';
+  static const String updateCurrentUserNick = 'updateCurrentUserNick';
 
-static const String init                        = 'init';
-static const String chatManager                 = 'chatManager';
-static const String register                    = 'register';
-static const String login                       = 'login';
-static const String loginWithToken              = 'loginWithToken';
-static const String logout                      = 'logout';
-static const String changeAppKey                = 'changeAppKey';
-static const String getCurrentUser              = 'getCurrentUser';
-static const String getUserTokenFromServer      = 'getUserTokenFromServer';
-static const String setDebugMode                = 'setDebugMode';
-static const String updateCurrentUserNick       = 'updateCurrentUserNick';
+  static const String sendMessage = 'sendMessage';
 
-static const String sendMessage                 = 'sendMessage';
-
-static const String onConnectionDidChanged      = 'onConnectionDidChanged';
+  static const String onConnectionDidChanged = 'onConnectionDidChanged';
 }
