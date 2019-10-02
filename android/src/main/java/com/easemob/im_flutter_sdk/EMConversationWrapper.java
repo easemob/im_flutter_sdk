ed00d9686e71bb04274116931a22374fceb1fefa
bingo <bingo.zhao@gmail.com>
2019-10-02 23:10:39 +0800

    fix android side async result.success() callback

--- android/src/main/java/com/easemob/im_flutter_sdk/EMConversationWrapper.java
+++ android/src/main/java/com/easemob/im_flutter_sdk/EMConversationWrapper.java
@@ -4,7 +4,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-public class EMConversationWrapper implements MethodCallHandler{
+public class EMConversationWrapper implements MethodCallHandler, EMWrapper{
     @Override
     public void onMethodCall(MethodCall call, Result result) {
         if (call.method.equals("getPlatformVersion")) {
