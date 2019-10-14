870e4ff03d22d21816eb501cd85fde7c85163ada
dujiepeng <347302029@qq.com>
2019-10-14 23:19:53 +0800

    添加ios调试输出

--- ios/Classes/ImFlutterSdkPlugin.m
+++ ios/Classes/ImFlutterSdkPlugin.m
@@ -4,20 +4,17 @@
 #import "EMClientWrapper.h"
 #import "EMContactManagerWrapper.h"
 #import "EMConversationWrapper.h"
+#import "EMLogWrapper.h"
 
 #define EMChannelName(name) [NSString stringWithFormat:@"com.easemob.im/%@",name]
 
 @implementation ImFlutterSdkPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-
     [[EMClientWrapper alloc] initWithChannelName:EMChannelName(@"em_client") registrar:registrar];
-    
     [[EMChatManagerWrapper alloc] initWithChannelName:EMChannelName(@"em_chat_manager") registrar:registrar];
-    
     [[EMContactManagerWrapper alloc] initWithChannelName:EMChannelName(@"em_contact_manager") registrar:registrar];
-    
     [[EMConversationWrapper alloc] initWithChannelName:EMChannelName(@"em_conversation") registrar:registrar];
-
+    [EMLogWrapper registrar:registrar];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
