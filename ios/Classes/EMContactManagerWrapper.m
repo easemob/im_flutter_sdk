8c8eb87d56ec80b73f4eafe1481f7244257f83bc
dujiepeng <347302029@qq.com>
2019-10-16 23:04:17 +0800

    update ios files

--- ios/Classes/EMContactManagerWrapper.m
+++ ios/Classes/EMContactManagerWrapper.m
@@ -6,13 +6,50 @@
 //
 
 #import "EMContactManagerWrapper.h"
+#import <Hyphenate/Hyphenate.h>
+
+@interface EMContactManagerWrapper () <EMContactManagerDelegate>
+
+@end
 
 @implementation EMContactManagerWrapper
 - (instancetype)initWithChannelName:(NSString *)aChannelName
                           registrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     if(self = [super initWithChannelName:aChannelName
                                    registrar:registrar]) {
+        
+        [EMClient.sharedClient.contactManager addDelegate:self delegateQueue:nil];
     }
     return self;
+}
+
+
+#pragma mark - FlutterPlugin
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    
+    result(FlutterMethodNotImplemented);
+}
+
+#pragma mark - EMContactManagerDelegate
+
+- (void)friendRequestDidApproveByUser:(NSString *)aUsername {
+    
+}
+
+- (void)friendRequestDidDeclineByUser:(NSString *)aUsername {
+    
+}
+
+- (void)friendshipDidRemoveByUser:(NSString *)aUsername {
+    
+}
+
+- (void)friendshipDidAddByUser:(NSString *)aUsername {
+    
+}
+
+- (void)friendRequestDidReceiveFromUser:(NSString *)aUsername
+                                message:(NSString *)aMessage {
+    
 }
 @end
