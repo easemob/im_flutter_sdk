01e27c776cdd216e5baebc8a1818ce9dd3011c67
dujiepeng <347302029@qq.com>
2019-10-17 11:32:37 +0800

    ios 添加groupManager, chatroomManager

---
+++ ios/Classes/EMChatroomManagerWrapper.h
@@ -0,0 +1,17 @@
+//
+//  EMChatroomManagerWrapper.h
+//  FlutterTest
+//
+//  Created by 杜洁鹏 on 2019/10/17.
+//  Copyright © 2019 Easemob. All rights reserved.
+//
+
+#import "EMWrapper.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EMChatroomManagerWrapper : EMWrapper
+
+@end
+
+NS_ASSUME_NONNULL_END
