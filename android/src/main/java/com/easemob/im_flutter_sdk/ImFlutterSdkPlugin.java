8dcad8d52f2ae4ffc8739936892ebb553c5f44ff
wangxinjeff <445044213@qq.com>
2019-10-18 14:23:28 +0800

    添加EMGroupManagerWrapper

--- android/src/main/java/com/easemob/im_flutter_sdk/ImFlutterSdkPlugin.java
+++ android/src/main/java/com/easemob/im_flutter_sdk/ImFlutterSdkPlugin.java
@@ -4,9 +4,12 @@ import android.os.Handler;
 import android.os.Looper;
 
 import com.hyphenate.EMCallBack;
+import com.hyphenate.EMValueCallBack;
 import com.hyphenate.exceptions.HyphenateException;
 import com.hyphenate.chat.EMClient;
 
+import io.flutter.plugin.common.JSONMessageCodec;
+import io.flutter.plugin.common.JSONMethodCodec;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.MethodChannel.Result;
@@ -28,26 +31,39 @@ public class ImFlutterSdkPlugin {
     registerChatManagerWith(registrar);
     registerContactManagerWith(registrar);
     registerConversationWith(registrar);
+    registerGroupManagerWith(registrar);
+    registerGroupWith(registrar);
   }
 
   public static void registerClientWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_client");
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_client", JSONMethodCodec.INSTANCE);
     channel.setMethodCallHandler(new EMClientWrapper(registrar.context(), channel));
   }
 
   public static void registerChatManagerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_chat_manager");
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_chat_manager", JSONMethodCodec.INSTANCE);
     channel.setMethodCallHandler(new EMChatManagerWrapper(channel));
   }
 
   public static void registerContactManagerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_contact_manager");
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_contact_manager", JSONMethodCodec.INSTANCE);
     channel.setMethodCallHandler(new EMContactManagerWrapper(channel));
   }
 
   public static void registerConversationWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_conversation");
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_conversation", JSONMethodCodec.INSTANCE);
     channel.setMethodCallHandler(new EMConversationWrapper());
+  }
+
+  public static void registerGroupManagerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_group_manager", JSONMethodCodec.INSTANCE);
+    channel.setMethodCallHandler(new EMGroupManagerWrapper(channel));
+  }
+
+  public static void registerGroupWith(Registrar registrar) {
+
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_PREFIX + "/em_group", JSONMethodCodec.INSTANCE);
+    channel.setMethodCallHandler(new EMGroupWrapper());
   }
 }
 
@@ -98,6 +114,17 @@ class EMWrapperCallBack implements EMCallBack{
       Map<String, Object> data = new HashMap<String, Object>();
       data.put("success", Boolean.TRUE);
       result.success(data);
+
+        new Thread(new Runnable() {
+          @Override
+          public void run() {
+            try {
+            EMClient.getInstance().groupManager().getJoinedGroupsFromServer();
+            }catch(Exception e){
+              e.printStackTrace();
+            }
+          }
+        }).start();
     });
   }
 
@@ -115,5 +142,42 @@ class EMWrapperCallBack implements EMCallBack{
   @Override
   public void onProgress(int progress, String status) {
     // no need
+  }
+}
+
+class EMValueWrapperCallBack<T> implements EMValueCallBack<T> {
+
+  EMValueWrapperCallBack(MethodChannel.Result result) {
+    this.result = result;
+  }
+  private MethodChannel.Result result;
+
+  void post(Consumer<Void> func) {
+    ImFlutterSdkPlugin.handler.post(new Runnable() {
+      @Override
+      public void run() {
+        func.accept(null);
+      }
+    });
+  }
+
+  @Override
+  public void onSuccess(Object value) {
+    post((Void)->{
+      Map<String, Object> data = new HashMap<String, Object>();
+      data.put("success", Boolean.TRUE);
+      result.success(data);
+    });
+  }
+
+  @Override
+  public void onError(int error, String errorMsg) {
+    post((Void)->{
+      Map<String, Object> data = new HashMap<String, Object>();
+      data.put("success", Boolean.FALSE);
+      data.put("code", error);
+      data.put("desc", errorMsg);
+      result.success(data);
+    });
   }
 }
