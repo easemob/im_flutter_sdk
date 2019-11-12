8f958891cbb26881ce269c9256591142d41e1123
wangxinjeff <445044213@qq.com>
2019-11-12 11:38:59 +0800

    增加导航栏

--- example/lib/utils/style.dart
+++ example/lib/utils/style.dart
@@ -1,11 +1,27 @@
-class EMLayout{
+class EMLayout {
+  //会话列表页面布局
+  static const double EMConListPortraitSize = 45; //会话列表头像大小
+  static const double EMConListItemHeight = 70; //会话列表 item 高度
+  static const double EMConListUnreadSize = 10; //会话列表未读数大小
 
 }
 
 class EMFont{
+  //会话列表相关字体大小
+  static const double EMConListNameFont = 16;
+  static const double EMConListTimeFont = 12;
+  static const double EMConListContentFont = 12;
+
 
 }
 
 class EMColor{
+  //会话列表相关颜色
+  static const EMConListTitleColor = 0xff000000;
+  static const EMConListContentColor = 0xff6C7B8B;
+  static const EMConListUnreadColor = 0xffCD3333;
+  static const EMConListTimeColor = 0xff6C7B8B;
+  static const EMConListItemBgColor = 0xffffffff;
+  static const EMConListBorderColor = 0xff6C7B8B;
 
 }
