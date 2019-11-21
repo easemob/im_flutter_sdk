d2e01ef1c6b25e7b900a7775c7fdfc207e3c0886
傅东海 <296322762@qq.com>
2019-11-21 23:19:13 +0800

    更新

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,9 +1,6 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-<<<<<<< HEAD
-=======
-#import <Hyphenate/Hyphenate.h>
->>>>>>> 增加:获取好友列表,添加好友(目前默认自动同意好友请求),删除好友，点击好友进入聊天页面
+
 #import <im_flutter_sdk/ImFlutterSdkPlugin.h>
 
 @implementation AppDelegate
