4589002cabe6d76f93c20f5bbf95cb729014fc1a
wangxinjeff <445044213@qq.com>
2019-11-22 16:26:58 +0800

    添加群组列表、公开群组列表

---
+++ example/lib/pages/public_group_list_page.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+import 'package:im_flutter_sdk/im_flutter_sdk.dart';
+import 'package:im_flutter_sdk_example/common/common.dart';
+import 'package:im_flutter_sdk_example/utils/widget_util.dart';
+import 'package:im_flutter_sdk_example/widgets/progress_dialog.dart';
+
+import 'package:im_flutter_sdk_example/utils/localizations.dart';
+import 'package:im_flutter_sdk_example/utils/theme_util.dart';
+import 'package:im_flutter_sdk_example/utils/style.dart';
+
+import 'chat_page.dart';
+
+class PublicGroupListPage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _PublicGroupListPageState();
+  }
+}
+
+class _PublicGroupListPageState extends State<PublicGroupListPage>{
+
+  var groupList = List<EMGroupInfo>();
+  bool _loading = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getPublicGroups();
+  }
+
+  void _getPublicGroups(){
+    EMClient.getInstance().groupManager().getPublicGroupsFromServer(pageSize: 20, cursor: '',
+    onSuccess: (result){
+      groupList = result.getData();
+      _loading = false;
+      _refreshUI();
+    },
+    onError: (code, desc){
+      WidgetUtil.hintBoxWithDefault(code.toString()+':'+desc);
+    });
+  }
+
+  _refreshUI(){
+    setState(() {
+
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  Widget _buildChatRoomListView(){
+    return ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: groupList.length,
+        itemBuilder:(BuildContext context, int index){
+          if(groupList.length <= 0){
+            return WidgetUtil.buildEmptyWidget();
+          }
+          return InkWell(
+              onTap: (){
+                _onTap(groupList[index]);
+            },
+            child: Container(
+              height: EMLayout.emContactListItemHeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _buildPortrait(),
+                  _buildContent(groupList[index]),
+                  ],
+                ),
+              ),
+            );
+        }
+    );
+  }
+
+  Widget _buildPortrait(){
+    return Stack(
+      overflow: Overflow.visible,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            SizedBox(
+              width: 8,
+            ),
+            ClipOval(
+              child: Container(
+                height: EMLayout.emContactListPortraitSize,
+                width: EMLayout.emContactListPortraitSize,
+                child: Image.asset('images/group_icon.png'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildContent(groupInfo){
+    return Expanded(
+      child: Container(
+          height: EMLayout.emContactListItemHeight,
+          margin: EdgeInsets.only(left:10, right: 10),
+          decoration:  BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(width: 0.5, color: ThemeUtils.isDark(context) ? EMColor.darkBorderLine : EMColor.borderLine)
+              )
+          ),
+          child: Row(
+            children: <Widget>[
+              Text(groupInfo.getGroupName(), style: TextStyle(fontSize: EMFont.emConListTitleFont),),
+            ],
+          )
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle : true,
+        backgroundColor: ThemeUtils.isDark(context) ? EMColor.darkAppMain : EMColor.appMain,
+        title: Text(DemoLocalizations.of(context).publicGroups, style: TextStyle(color: ThemeUtils.isDark(context) ? EMColor.darkText : EMColor.text)),
+      ),
+      key: UniqueKey(),
+      body: Stack(children: <Widget>[
+        _buildChatRoomListView(),
+        ProgressDialog(loading: _loading, msg: DemoLocalizations.of(context).loading,),
+      ],
+      ),
+    );
+  }
+
+  void _onTap(EMGroupInfo groupInfo){
+    EMClient.getInstance().groupManager().getGroupFromServer(groupId:groupInfo.getGroupId(),
+        onSuccess: (group){
+          if(group.isMemberOnly()) {
+            EMClient.getInstance().groupManager().applyJoinToGroup(groupId: group.getGroupId(), reason: '',
+              onSuccess: (){
+                WidgetUtil.hintBoxWithDefault('申请加入公开群成功，等待群主同意');
+              },
+              onError: (code, desc){
+                WidgetUtil.hintBoxWithDefault('申请加入公开群失败：'+desc);
+              });
+          }else{
+            EMClient.getInstance().groupManager().joinGroup(groupId: group.getGroupId(),
+            onSuccess: (){
+              WidgetUtil.hintBoxWithDefault('加入公开群成功');
+              Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context){
+                return new ChatPage(arguments: {'mType': Constant.chatTypeGroup,'toChatUsername':group.getGroupId()});
+              }));
+            },
+            onError: (code, desc){
+              WidgetUtil.hintBoxWithDefault('加入公开群失败：'+desc);
+            }
+            );
+          }
+        },
+        onError: (code, desc){
+          WidgetUtil.hintBoxWithDefault('获取群组详情失败：'+desc);
+        }
+    );
+  }
+}
--- lib/src/em_domain_terms.dart
+++ lib/src/em_domain_terms.dart
@@ -574,17 +574,25 @@ abstract class EMCursorResults<T> {
   Future<T> getCursor();
 }
 
-class EMCursorResult {
+class EMCursorResult<T> {
   String _cursor;
 
-  List _data;
+  List<T> _data;
 
   String getCursor(){
     return _cursor;
   }
 
-  List getData(){
+  void setCursor(String cursor){
+    _cursor = cursor;
+  }
+
+  List<T> getData(){
     return _data;
+  }
+
+  void setData(List list){
+    _data = list;
   }
 
   EMCursorResult.from(Map<String, dynamic> data)
@@ -599,6 +607,10 @@ class EMPageResult<T>{
 
   int getPageCount() {
     return _pageCount;
+  }
+
+  void setPageCount(int pageCount) {
+    _pageCount = pageCount;
   }
 
   List<T> getData(){
