2cec30a43be9db88913d4d41ce8d6eb20af21578
wangxinjeff <445044213@qq.com>
2019-12-19 11:22:45 +0800

    flutter demo添加Channel通知native层登录成功

---
+++ example/android/app/src/main/java/com/easemob/im_flutter_sdk_example/ImDemoFlugin.java
@@ -0,0 +1,34 @@
+package com.easemob.im_flutter_sdk_example;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
+
+public class
+ImDemoFlugin implements MethodChannel.MethodCallHandler {
+    public static final String CHANNEL = "com.easemob.demo/plugin";
+    static MethodChannel channel;
+
+    private ImDemoFlugin(){}
+
+    public static void registerWith(PluginRegistry.Registrar registrar) {
+        channel = new MethodChannel(registrar.messenger(), CHANNEL);
+        ImDemoFlugin imDemoFlugin = new ImDemoFlugin();
+        channel.setMethodCallHandler(imDemoFlugin);
+    }
+
+
+    @Override
+    public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+        if(methodCall.method.equals("loginComplete")){
+            /***
+             * 已登录成功进入主界面，去注册华为推送，实现方式和Android demo一样，
+             * 需要在PushReceiver#onToken里调用
+             * EMClient.getInstance().sendHMSPushTokenToServer(token)
+             * 去上传推送token
+             */
+
+            result.success("success");
+        }
+    }
+}
