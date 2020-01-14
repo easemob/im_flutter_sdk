7a54e64103d7ef4c9a2d06a334a5ed0c49916282
傅东海 <296322762@qq.com>
2020-01-14 18:05:04 +0800

    iOS:1v1音视频通话拨打，接收实现(SDK相关方法待封装)

---
+++ ios/Classes/Call/1v1/Call1v1AudioViewController.h
@@ -0,0 +1,13 @@
+//
+//  Call1v1AudioViewController.h
+//  ChatDemo-UI3.0
+//
+//  Created by XieYajie on 2018/9/19.
+//  Copyright © 2018 XieYajie. All rights reserved.
+//
+
+#import "EM1v1CallViewController.h"
+
+@interface Call1v1AudioViewController : EM1v1CallViewController
+
+@end
---
+++ ios/Classes/Call/Helper/CallSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CallSettingViewController.h
+//  ChatDemo-UI3.0
+//
+//  Created by XieYajie on 06/12/2016.
+//  Copyright © 2016 XieYajie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CallSettingViewController : UITableViewController
+
+@end
