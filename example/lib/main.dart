b617a4c6219c975cae1c4ea0a6fd0a03b7dde9fb
dujiepeng <347302029@qq.com>
2021-02-03 15:53:13 +0800

    change appkey: easemob-demo#easeim

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,8 +14,8 @@ void main() {
 
   SystemUiOverlayStyle uiStyle = SystemUiOverlayStyle.light;
   SystemChrome.setSystemUIOverlayStyle(uiStyle);
-  EMPushConfig config = EMPushConfig()..enableAPNs('chatdemoui');
-  var options = EMOptions(appKey: 'easemob-demo#chatdemoui');
+  EMPushConfig config = EMPushConfig()..enableAPNs('EaseIM_APNS_Product');
+  var options = EMOptions(appKey: 'easemob-demo#easeim');
   options.pushConfig = config;
   EMClient.getInstance.init(options);
   return runApp(EaseIMDemo());
