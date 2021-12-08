0f8f5f60903c77af8e9b28195b824e246fb2c18a
dujiepeng <347302029@qq.com>
2021-12-08 16:08:36 +0800

    add inviter users to group manager

--- android/src/main/java/com/easemob/im_flutter_sdk/EMGroupManagerWrapper.java
+++ android/src/main/java/com/easemob/im_flutter_sdk/EMGroupManagerWrapper.java
@@ -78,6 +78,8 @@ public class EMGroupManagerWrapper extends EMWrapper implements MethodCallHandle
                 getGroupBlockListFromServer(param, EMSDKMethod.getGroupBlockListFromServer, result);
             } else if (EMSDKMethod.addMembers.equals(call.method)) {
                 addMembers(param, EMSDKMethod.addMembers, result);
+            } else if (EMSDKMethod.inviterUser.equals(call.method)){
+                inviterUser(param, EMSDKMethod.inviterUser, result);
             } else if (EMSDKMethod.removeMembers.equals(call.method)) {
                 removeMembers(param, EMSDKMethod.removeMembers, result);
             } else if (EMSDKMethod.blockMembers.equals(call.method)) {
@@ -335,6 +337,22 @@ public class EMGroupManagerWrapper extends EMWrapper implements MethodCallHandle
                 new EMValueWrapperCallBack<String>(result, channelName));
     }
 
+    private void inviterUser(JSONObject param, String channelName, Result result) throws JSONException {
+        String groupId = param.getString("groupId");
+        String reason = null;
+        if (param.has("reason")) {
+            reason = param.getString("reason");
+        }
+        JSONArray array = param.getJSONArray("members");
+        String[] members = new String[array.length()];
+        for (int i = 0; i < array.length(); i++) {
+            members[i] = array.getString(i);
+        }
+
+        EMClient.getInstance().groupManager().asyncInviteUser(groupId, members, reason,
+                new EMWrapperCallBack(result, channelName, true));
+    }
+
     private void addMembers(JSONObject param, String channelName, Result result) throws JSONException {
         String groupId = param.getString("groupId");
         JSONArray array = param.getJSONArray("members");
@@ -343,6 +361,7 @@ public class EMGroupManagerWrapper extends EMWrapper implements MethodCallHandle
         for (int i = 0; i < array.length(); i++) {
             members[i] = array.getString(i);
         }
+
 
         EMClient.getInstance().groupManager().asyncAddUsersToGroup(groupId, members,
                 new EMWrapperCallBack(result, channelName, true));
