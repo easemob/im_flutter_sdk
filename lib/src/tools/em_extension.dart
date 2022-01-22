52ca57539b8a5dc17aa3678607a92fd5acc01233
dujiepeng <347302029@qq.com>
2022-01-22 19:30:38 +0800

    fix group muteAll and block param error.

--- lib/src/tools/em_extension.dart
+++ lib/src/tools/em_extension.dart
@@ -8,6 +8,8 @@ extension MapExtension on Map {
       return this[key] == 0 ? false : true;
     } else if (this[key] is String) {
       return this[key].toString().length == 0 ? false : true;
+    } else if (this[key] is bool) {
+      return this[key];
     } else {
       return false;
     }
