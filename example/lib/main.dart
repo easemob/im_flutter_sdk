7b8e67ba09ab569638f2ab7c1c0695ef5d388c04
dujiepeng <347302029@qq.com>
2022-06-20 14:35:41 +0800

    update demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Agora Flutter SDK Demo'),
+      home: const MyHomePage(title: 'Flutter SDK Demo'),
     );
   }
 }
