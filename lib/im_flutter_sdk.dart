10cf896adbd3dda48cb8ffca01a8f93e28dcf75a
dujiepeng <347302029@qq.com>
2022-08-17 17:58:32 +0800

    add listener callbacks.

--- lib/im_flutter_sdk.dart
+++ lib/im_flutter_sdk.dart
@@ -35,5 +35,5 @@ export 'src/models/em_download_callback.dart' show EMDownloadCallback;
 export 'src/models/em_message_reaction.dart';
 export 'src/models/em_chat_thread.dart';
 export 'src/models/chat_silent_mode.dart';
-
+export 'src/callbacks/em_listener_callbacks.dart';
 export 'src/em_message_status_callback.dart' show MessageStatusCallBack;
