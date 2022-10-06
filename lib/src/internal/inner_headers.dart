deed63114a2575602a8046e71c317e0aef01074d
dujiepeng <347302029@qq.com>
2022-10-06 16:05:05 +0800

    rename area class name.

--- lib/src/internal/inner_headers.dart
+++ lib/src/internal/inner_headers.dart
@@ -45,4 +45,4 @@ export '../em_push_manager.dart';
 export '../em_userInfo_manager.dart';
 export '../em_push_manager.dart';
 export '../event_handler/manager_event_handler.dart';
-export '../tools/em_area_code.dart';
+export '../tools/area_code.dart';
