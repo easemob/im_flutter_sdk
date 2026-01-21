b3fa3c292ed83c91ac5958ba0a830409dfcf7561
luckyyuqiang <luckyyuqiang@163.com>
2026-01-21 17:50:18 +0800

    set user_agent for flutter on ios and android.

--- im_flutter_sdk_android/android/src/main/java/com/easemob/im_flutter_sdk/EMHelper.java
+++ im_flutter_sdk_android/android/src/main/java/com/easemob/im_flutter_sdk/EMHelper.java
@@ -69,6 +69,9 @@ class OptionsHelper {
         }else {
             options.setAppId(json.optString("appId"));
         }
+
+        options.setSDKPlatform(EMOptions.EMSDKPlatform.EMSDKPlatformFlutter);
+
         options.setAutoLogin(json.getBoolean("autoLogin"));
         options.setRequireAck(json.getBoolean("requireAck"));
         options.setRequireDeliveryAck(json.getBoolean("requireDeliveryAck"));
